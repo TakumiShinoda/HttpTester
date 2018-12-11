f8a7a095df641c651fb187256958e93e14a4e1ac
TakimiShinoda <godgodx4@gmail.com>
2018-12-11 18:38:26 +0900

    update looks

--- src/assets/typescript/index/index.ts
+++ src/assets/typescript/index/index.ts
@@ -6,27 +6,24 @@ import '../../css/index/styles.css'
 
 const {distPath, srcPath} = require('../../../../dev/path.js')
 
-$(document).ready(() => {
-  let webviewExm1: any = $('#webviewExm1')[0]; 
-  let module: Test = new Test() 
+function show_hide(ele: string, open: boolean= true){
+  let element: JQuery<HTMLElement> = $(ele);
 
-  module.logMes()
-  module.logMes('hoge')
-
-  webviewSetJs(webviewExm1);
-  // webviewExm1.openDevTools();
-});
-
-function webviewSetJs(ele: any){
-  ele.addEventListener('dom-ready', () => {
-    $.get('../../js/index/webviewResources/webviewExm1/domReady.js', (text: string) => {
-      ele.executeJavaScript(text, false, () => {});
-    });
-  });
-
-  ele.addEventListener('did-finish-load', () => {
-    $.get('../../js/index/webviewResources/webviewExm1/didFinishLoad.js', (text: string) => {
-      ele.executeJavaScript(text, false, () => {});
-    });
-  });
+  if(open) element.css('display', 'block');
+  else element.css('display', 'none');
 }
+
+$(document).ready(() => {
+  show_hide('#home', true);
+  show_hide('#add', false);
+
+  $('#menu_home').on('click', () => {
+    show_hide('#home', true);
+    show_hide('#add', false);
+  });
+
+  $('#menu_add').on('click', () => {
+    show_hide('#home', false);
+    show_hide('#add', true);
+  });
+});
