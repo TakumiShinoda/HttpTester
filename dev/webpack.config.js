875f43ad80b175a3ce8862b186f2107ac51bc346
TakimiShinoda <godgodx4@gmail.com>
2019-01-03 19:07:39 +0900

    fix build speed

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -1,3 +1,4 @@
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const {srcPath, distPath} = require('./path');
 
 module.exports = {
@@ -9,6 +10,9 @@ module.exports = {
         filename: routes + ".js"
       },
       target: "electron-renderer",
+      plugins: [
+        new HardSourceWebpackPlugin()
+      ],    
       module: {
         rules: [
           {
